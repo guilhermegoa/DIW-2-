45d5bfe94f7779d17cc7c62003567b2a3d48fcdc
Guilherme <guilhergoa@hotmail.com>
2019-06-17 18:31:45 -0300

    comentario

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,10 @@
 $(document).ready(function () {
 
+
   //Caso logado vai para tela principal
   if (localStorage.getItem('logado') == 'sim') {
-    $('#tela-login').fadeOut('fast');
-    $('#tela-principal').fadeIn();
+    $('#tela-login').hide();
+    $('#tela-principal').show();
     carregaLocalStorage();
     carregaCards();
   }
@@ -20,7 +21,7 @@ $(document).ready(function () {
   function carregaCards() {
     dbGoa = JSON.parse(localStorage.getItem('dbGuilhermeOliveira'));
     $('#tela-principal').html('')
-    for (i = dbGoa.length -1; i >= 0; i--) {
+    for (i = dbGoa.length - 1; i >= 0; i--) {
       $('#tela-principal').append(`<ul>
             <a href="#" type="button">
             <li class="principal-card" noticia="${[i]}">
@@ -39,8 +40,10 @@ $(document).ready(function () {
   };
 
   //Carrega post
-  function carregaPost(noticia) {
+  function carregaPost() {
     dbGoa = JSON.parse(localStorage.getItem('dbGuilhermeOliveira'));
+    noticia = localStorage.getItem('noticia')
+    $('#tela-post').html('')
     $('#tela-post').append(`<div class="post-post">
         <h2>${dbGoa[noticia].titulo}</h2>
         <img src="${dbGoa[noticia].imagem}" alt="" />
@@ -48,122 +51,170 @@ $(document).ready(function () {
       </div>
       <section id="comentarios">
         <div class="comentario-like">
-          <a type="button" id="botao-like">
-            <div class="like"><img
+          <a type="button" >
+            <div class="like"><img id="botao-like"
               src="https://cdn4.iconfinder.com/data/icons/like-18/32/459-01-512.png"
               alt="likeImg"/></a>
              <p>${dbGoa[noticia].like}</p>
             </div>
           <div><spam id="data-post">${dbGoa[noticia].data}</span></div>
         </div>
-        <div class="coment-coment">
-          <div id="comentario-carrega"></div>
-          <textarea
-            name=""
-            id="novo-comentario"
-            cols="100"
-            rows="10"
-          ></textarea>
-          <div><button id="comentario-postar">Comentar</button></div>
-        </div>
       </section>`);
+      
+  }
+
+  //Funçao carrega comentario   
+  function carregaComentario() {
+    $('#comentario-carrega').html('')
+    for (i = dbGoa[noticia].comentario.length - 1; i >= 0; i--) {
+      let coment = dbGoa[noticia].comentario[i].comentario;
+      $('#comentario-carrega').append(`<p>${coment}</p>`);
+    }
+  }
+
+  //Funçao novo comentario
+  function novoComentario() {
+    $('#novo-comentario').append(`<div class="coment-coment">
+    <textarea
+      name=""
+      id="criar-comentario"
+      cols="100"
+      rows="10"
+    ></textarea>
+    <div><button id="comentario-postar">Comentar</button></div>
+  </div>`)
   }
 
   //EVENTOS
-  $('#login-cadastro').on('click', function () {
-    $('#tela-login').fadeOut();
-    $('#tela-cadastro').fadeIn();
-  });
+  function adicionarEventos() {
 
-  $('#cadastro-confirmar').on('click', function () {
-    let usuario = {
-      nome: $('#cadastro-nome').val(),
-      senha: $('#cadastro-senha').val()
-    };
+    $('#login-cadastro').on('click', function () {
+      $('#tela-login').hide();
+      $('#tela-cadastro').show();
+    });
 
-    if (!usuario.nome || !usuario.senha) {
-      alert('Há campo em branco');
-    } else {
-      $('#cadastro-nome').val('');
-      $('#cadastro-email').val('');
-      $('#cadastro-senha').val('');
-      localStorage.setItem('usuario', JSON.stringify(usuario));
-      $('#tela-cadastro').fadeOut();
-      $('#tela-login').fadeIn();
-    }
-  });
+    $('#cadastro-confirmar').on('click', function () {
+      let usuario = {
+        nome: $('#cadastro-nome').val(),
+        senha: $('#cadastro-senha').val()
+      };
 
-  $('#login-botao').on('click', function () {
-    usuario = localStorage.getItem('usuario');
-    usuario = JSON.parse(usuario);
-    let nome = $('#login-nome').val();
-    let senha = $('#login-senha').val();
+      if (!usuario.nome || !usuario.senha) {
+        alert('Há campo em branco');
+      } else {
+        $('#cadastro-nome').val('');
+        $('#cadastro-email').val('');
+        $('#cadastro-senha').val('');
+        localStorage.setItem('usuario', JSON.stringify(usuario));
+        $('#tela-cadastro').fadeOut();
+        $('#tela-login').fadeIn();
+      }
+    });
 
-    if (nome === usuario.nome && senha === usuario.senha) {
-      $('#tela-login').fadeOut();
-      $('#tela-principal').fadeIn();
-      localStorage.setItem('logado', 'sim');
-      carregaLocalStorage();
-      carregaCards();
-      location.reload();
-    } else {
-      alert("Senha/Nome digitados errados ou nao é cadastrado");
-    }
-  });
+    $('#login-botao').on('click', function () {
+      usuario = localStorage.getItem('usuario');
+      usuario = JSON.parse(usuario);
+      let nome = $('#login-nome').val();
+      let senha = $('#login-senha').val();
 
-  $('.principal-card').on('click', function (event) {
-    var noticia = $(event.currentTarget).attr('noticia');
-    localStorage.setItem('noticia', noticia);
-    $('#tela-principal').fadeOut();
-    $('#tela-post').fadeIn();
-    carregaPost(noticia);
-  });
+      if (nome === usuario.nome && senha === usuario.senha) {
+        $('#tela-login').hide();
+        $('#tela-principal').show();
+        localStorage.setItem('logado', 'sim');
+        carregaLocalStorage();
+        carregaCards();
+        location.reload();
+      } else {
+        alert("Senha/Nome digitados errados ou nao é cadastrado");
+      }
+    });
 
-  $('#header-botao').on('click', function () {
-    if (localStorage.getItem('logado') == 'sim') {
-      $('#tela-post').fadeOut();
-      $('#tela-principal').fadeOut();
-      $('#tela-criar-post').fadeIn();
-    }
-  })
+    $('.principal-card').on('click', function (event) {
+      var noticia = $(event.currentTarget).attr('noticia');
+      localStorage.setItem('noticia', noticia);
+      $('#tela-principal').hide();
+      $('#tela-post').show();
+      $('#comentario-carrega').show();
+      $('#novo-comentario').show();
+      carregaPost();
+      carregaComentario()
+      novoComentario();
+      eventoPostar();
+      eventoLike();
+    });
 
-  $('#criar-post-postar').on('click', function () {
-    let novoPost = {
-      autor: $('#criar-post-autor').val(),
-      titulo: $('#criar-post-titulo').val(),
-      descricao: $('#criar-post-descricao').val(),
-      imagem: $('#criar-post-autor').val(),
-      conteudo: $('#criar-post-conteudo').val(),
-      data: "",
-      like: "0",
-      comentario: []
-    };
+    $('#header-botao').on('click', function () {
+      if (localStorage.getItem('logado') == 'sim') {
+        $('#tela-post').hide();
+        $('#comentario-carrega').hide();
+        $('#novo-comentario').hide();
+        $('#tela-principal').hide();
+        $('#tela-criar-post').show();
+      }
+    })
 
-    JSON.stringify(novoPost);
+    $('#criar-post-postar').on('click', function () {
+      let novoPost = {
+        autor: $('#criar-post-autor').val(),
+        titulo: $('#criar-post-titulo').val(),
+        descricao: $('#criar-post-descricao').val(),
+        imagem: $('#criar-post-autor').val(),
+        conteudo: $('#criar-post-conteudo').val(),
+        data: "",
+        like: "0",
+        comentario: []
+      };
+      dbGoa = JSON.parse(localStorage.getItem('dbGuilhermeOliveira'));
+      JSON.stringify(novoPost);
 
-    if (!novoPost.autor || !novoPost.titulo || !novoPost.descricao || !novoPost.conteudo) {
-      alert('Há campos em branco');
-    } else {
-      $('#criar-post-autor').val('');
-      $('#criar-post-titulo').val('')
-      $('#criar-post-descricao').val('')
-      $('#criar-post-autor').val('');
-      $('#criar-post-conteudo').val('');
-      dbGoa.push(novoPost);
-      localStorage.setItem('dbGuilhermeOliveira', JSON.stringify(dbGoa));
-      $('#tela-criar-post').fadeOut();
-      $('#tela-principal').fadeIn();
-      location.reload();
-    }
-  })
+      if (!novoPost.autor || !novoPost.titulo || !novoPost.descricao || !novoPost.conteudo) {
+        alert('Há campos em branco');
+      } else {
+        $('#criar-post-autor').val('');
+        $('#criar-post-titulo').val('')
+        $('#criar-post-descricao').val('')
+        $('#criar-post-autor').val('');
+        $('#criar-post-conteudo').val('');
+        dbGoa.push(novoPost);
+        localStorage.setItem('dbGuilhermeOliveira', JSON.stringify(dbGoa));
+        $('#tela-criar-post').hide();
+        $('#tela-principal').show();
+        carregaCards();
+        adicionarEventos();
+      }
+    })
+  }
 
-  $('#comentario-postar').on('click', function () {
-    alert('apertou')
-  })
+  function eventoPostar() {
+    $('#comentario-postar').on('click', function () {
+      let comentario = {
+        comentario: $('#criar-comentario').val()
+      }
+      dbGoa = JSON.parse(localStorage.getItem('dbGuilhermeOliveira'));
+      noticia = localStorage.getItem('noticia')
 
-  $('#botao-like').on('click', function () {
-    alert('apertou');
-  })
+      if (!comentario.comentario) {
+        alert('Campo de comnetario esta em branco')
+      } else {
+        JSON.stringify(comentario)
+        dbGoa[noticia].comentario.push(comentario)
+        localStorage.setItem('dbGuilhermeOliveira', JSON.stringify(dbGoa));
+        $('#criar-comentario').val('');
+        carregaComentario();
+      }
 
+    })
 
-})
+  };
+
+  function eventoLike() {
+    $('#botao-like').on('click', function () {
+      alert('apertou');
+
+    })
+  }
+
+  //Rodar ao entrar
+  adicionarEventos();
+
+});
